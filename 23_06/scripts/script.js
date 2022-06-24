4819decf4768ceafd65bdbd995dfa276322d3dee
My Name <borisenko_07@mail.ru>
2022-06-24 15:35:58 +0200

    progress added and avatar in first place

--- 23_06/scripts/script.js
+++ 23_06/scripts/script.js
@@ -1,5 +1,3 @@
-//Добавьте к форме поле ввода прогресса. Выведите прогресс в карточку.
-
 const formElem = document.querySelector('.workers_form');
 
 let workersArr = [];
@@ -8,7 +6,17 @@ const cardsRender = () => {
 	const cardscontainer = document.querySelector('.cards_container');
 	cardscontainer.innerText = '';
 	workersArr.forEach(
-		({ firstname, lastname, age, rate, days, salary, avatar, email }) => {
+		({
+			firstname,
+			lastname,
+			age,
+			rate,
+			days,
+			salary,
+			avatar,
+			email,
+			progress,
+		}) => {
 			const container = document.createElement('div');
 			const firstnameElem = document.createElement('p');
 			const lastnameElem = document.createElement('p');
@@ -18,6 +26,7 @@ const cardsRender = () => {
 			const salaryElem = document.createElement('p');
 			const user_avatar = document.createElement('img');
 			const user_email = document.createElement('a');
+			const progressElem = document.createElement('div');
 
 			firstnameElem.innerText = `First name: ${firstname}`;
 			lastnameElem.innerText = `Last name: ${lastname}`;
@@ -27,16 +36,18 @@ const cardsRender = () => {
 			salaryElem.innerText = `Salary: ${rate * days}`;
 			user_avatar.setAttribute('src', avatar);
 			user_email.innerText = `E-mail: ${email}`;
+			progressElem.innerText = `Progress: ${progress}`;
 
 			container.append(
+				user_avatar,
 				firstnameElem,
 				lastnameElem,
 				ageElem,
 				rateElem,
 				daysElem,
 				salaryElem,
-				user_avatar,
-				user_email
+				user_email,
+				progressElem
 			);
 			cardscontainer.append(container);
 		}
@@ -45,8 +56,17 @@ const cardsRender = () => {
 
 formElem.addEventListener('submit', (event) => {
 	event.preventDefault();
-	const { firstname, lastname, age, rate, days, salary, avatar, email } =
-		event.target; //this ксли не стрелочная
+	const {
+		avatar,
+		firstname,
+		lastname,
+		age,
+		rate,
+		days,
+		salary,
+		email,
+		progress,
+	} = event.target; //this ксли не стрелочная
 	workersArr.push({
 		firstname: firstname.value,
 		lastname: lastname.value,
@@ -56,6 +76,7 @@ formElem.addEventListener('submit', (event) => {
 		salary: salary.value,
 		avatar: avatar.value,
 		email: email.value,
+		progress: progress.value,
 	});
 	firstname.value = '';
 	lastname.value = '';
@@ -64,6 +85,8 @@ formElem.addEventListener('submit', (event) => {
 	days.value = '';
 	salary.value = '';
 	avatar.value = '';
-	(email.value = ''), console.log(workersArr);
+	email.value = '';
+	progress.value = '';
+	console.log(workersArr);
 	cardsRender();
 });
