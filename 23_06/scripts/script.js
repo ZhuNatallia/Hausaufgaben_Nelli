981c572e92ada1cc784ffd3524ec1b504f89f08e
My Name <borisenko_07@mail.ru>
2022-06-24 12:51:57 +0200

    form done

--- 23_06/scripts/script.js
+++ 23_06/scripts/script.js
@@ -2,6 +2,24 @@ const formElem = document.querySelector('.workers_form');
 
 let workersArr = [];
 
+const cardsRender = () => {
+	const cardscontainer = document.querySelector('.cards_container');
+	cardscontainer.innerText = '';
+	workersArr.forEach(({ firstname, lastname, age }) => {
+		const container = document.createElement('div');
+		const firstnameElem = document.createElement('p');
+		const lastnameElem = document.createElement('p');
+		const ageElem = document.createElement('p');
+
+		firstnameElem.innerText = `First name: ${firstname}`;
+		lastnameElem.innerText = `Last name: ${lastname}`;
+		ageElem.innerText = `Age: ${age}`;
+
+		container.append(firstnameElem, lastnameElem, ageElem);
+		cardscontainer.append(container);
+	});
+};
+
 formElem.addEventListener('submit', (event) => {
 	event.preventDefault();
 	const { firstname, lastname, age } = event.target; //this ксли не стрелочная
@@ -14,4 +32,5 @@ formElem.addEventListener('submit', (event) => {
 	lastname.value = '';
 	age.value = '';
 	console.log(workersArr);
+	cardsRender();
 });
