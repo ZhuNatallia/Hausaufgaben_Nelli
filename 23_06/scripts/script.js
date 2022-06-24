654eecb47b80e9fb59c8be1e932d6b58f6488c09
My Name <borisenko_07@mail.ru>
2022-06-24 14:28:37 +0200

    rate and days added

--- 23_06/scripts/script.js
+++ 23_06/scripts/script.js
@@ -1,3 +1,11 @@
+//Добавьте к форме поля ввода для ставки (rate), количества дней (days) и добавьте в карточку работника его зарплату. Зарплата рассчитывается через умножение ставки на количество дней.
+
+//Добавьте к форме поле ввода для ссылки на фото. Карточки должны включать отображение фотографии.
+
+//Добавьте к форме поле ввода почты. Карточки должны включать отображение фотографии.
+
+//Добавьте к форме поле ввода прогресса. Выведите прогресс в карточку.
+
 const formElem = document.querySelector('.workers_form');
 
 let workersArr = [];
@@ -5,32 +13,40 @@ let workersArr = [];
 const cardsRender = () => {
 	const cardscontainer = document.querySelector('.cards_container');
 	cardscontainer.innerText = '';
-	workersArr.forEach(({ firstname, lastname, age }) => {
+	workersArr.forEach(({ firstname, lastname, age, rate, days }) => {
 		const container = document.createElement('div');
 		const firstnameElem = document.createElement('p');
 		const lastnameElem = document.createElement('p');
 		const ageElem = document.createElement('p');
+		const rateElem = document.createElement('p');
+		const daysElem = document.createElement('p');
 
 		firstnameElem.innerText = `First name: ${firstname}`;
 		lastnameElem.innerText = `Last name: ${lastname}`;
 		ageElem.innerText = `Age: ${age}`;
+		rateElem.innerText = `Rate: ${rate}`;
+		daysElem.innerText = `Days: ${days}`;
 
-		container.append(firstnameElem, lastnameElem, ageElem);
+		container.append(firstnameElem, lastnameElem, ageElem, rateElem, daysElem);
 		cardscontainer.append(container);
 	});
 };
 
 formElem.addEventListener('submit', (event) => {
 	event.preventDefault();
-	const { firstname, lastname, age } = event.target; //this ксли не стрелочная
+	const { firstname, lastname, age, rate, days } = event.target; //this ксли не стрелочная
 	workersArr.push({
 		firstname: firstname.value,
 		lastname: lastname.value,
 		age: age.value,
+		rate: rate.value,
+		days: days.value,
 	});
 	firstname.value = '';
 	lastname.value = '';
 	age.value = '';
+	rate.value = '';
+	days.value = '';
 	console.log(workersArr);
 	cardsRender();
 });
